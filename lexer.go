17e2b4912b433708d4e654c3f7ac015590bb6b0d
Lily Appely <lappee@yahoo.com>
2023-03-24 17:41:55 +0300

    [REFACTORING] :: Refactor lex function and chop it at some functions

--- lexer.go
+++ lexer.go
@@ -51,41 +51,54 @@ func isTokAtStart(in string, matcher *regexp.Regexp) bool {
 	return matcher.FindStringIndex(in) != nil && matcher.FindStringIndex(in)[0] == 0
 }
 
+func tryMatch(in string, matcher *regexp.Regexp) (string, int, bool) {
+	if isTokAtStart(in, matcher) {
+		value := matcher.FindString(in)
+		nextPos := matcher.FindStringIndex(in)[1]
+
+		return value, nextPos, true
+	}
+	return "", 0, false
+}
+
+func throwIllegalTokenErr(in string, pos int) error {
+	var errorMsg string
+	afterPosLen := len(in[pos:])
+
+	if afterPosLen >= 3 {
+		afterPosLen = 3
+	}
+
+	errorMsg = fmt.Sprintf(
+		"Illegal token at position: %d, %s...", pos, in[pos:afterPosLen])
+	return errors.New(errorMsg)
+}
+
 func (this *Lexer) Lex(in string) (tokens []Token, err error) {
 	position := 0
 
 	for position < len(in) {
 		in = in[position:]
 		match := false
-		nextTokPos := 0
 
 		for _, exp := range this.Expressions {
 			matcher, tag := exp.matcher, exp.tag
+			value, nextPos, isMatched := tryMatch(in, matcher)
 
-			if isTokAtStart(in, matcher) {
+			if isMatched {
 				match = true
-				nextTokPos = matcher.FindStringIndex(in)[1]
-
+				position = nextPos
 				if tag != this.IgnoreTokName {
-					tokens = append(tokens, Token{
-						Value: matcher.FindString(in),
-						Tag:   tag,
-					})
+					tokens = append(tokens, Token{value, tag})
 				}
-
 				break
 			}
 			match = false
 		}
 
 		if !match {
-			err = errors.New(fmt.Sprintf("Illegal token at position: %d, %s", position, in[position:position+3]))
-			tokens = nil
-			return
+			return nil, throwIllegalTokenErr(in, position)
 		}
-
-		position = nextTokPos
 	}
-
 	return tokens, nil
 }
